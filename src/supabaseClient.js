30a2b1b0ebbb2a2fdcda7d015ae2008b965a9b7e
dipar2848-byte <dipar2848@gmail.com>
2026-05-25 13:35:28 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "PASTE_YOUR_URL"
+const supabaseUrl = "https://afgounopvxrmyghtmdmk.supabase.co"
 const supabaseKey = "sb_publishable_lDGn1V8iXeU3BwaTctM86w_ybASwOTM"
 export const supabase = createClient(supabaseUrl, supabaseKey)
