9a3c45e4a8aed35bfb62ad3d3acfd1b4d732c0b2
dipar2848-byte <dipar2848@gmail.com>
2026-05-26 13:02:36 +0530

    Create AdminLogin.jsx

---
+++ src/admin/AdminLogin.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import { supabase } from '../supabase'
+import { useNavigate } from 'react-router-dom'
+
+export default function AdminLogin() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  const login = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    })
+
+    if (error) return alert(error.message)
+
+    navigate('/admin/dashboard')
+  }
+
+  return (
+    <div style={{ padding: 40, color: 'white' }}>
+      <h2>Admin Login</h2>
+
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <br />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <br />
+
+      <button onClick={login}>Login</button>
+    </div>
+  )
+}
