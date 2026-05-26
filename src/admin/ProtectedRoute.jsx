163bc888ebc76932b87fb77c537aa4eb6ae288b5
dipar2848-byte <dipar2848@gmail.com>
2026-05-26 17:40:15 +0530

    Update ProtectedRoute.jsx

--- src/admin/ProtectedRoute.jsx
+++ src/admin/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { supabase } from '../supabase'
+import { supabase } from '../supabaseClient'
 import { Navigate } from 'react-router-dom'
 
 export default function ProtectedRoute({ children }) {
