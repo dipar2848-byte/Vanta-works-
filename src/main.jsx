9449a058a25e0105f633ed73fc8c6a4f724e10bf
dipar2848-byte <dipar2848@gmail.com>
2026-05-28 15:35:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './styles.css'
+import "./styles/base.css";
+import "./styles/main.css";
+import "./styles/crm.css";
+import "./styles/admin.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
